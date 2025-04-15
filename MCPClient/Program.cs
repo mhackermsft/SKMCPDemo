82cc7ff3f5d0e1a547d97fd9c88b408c98e71b39
Mike Hacker <61195478+mhackermsft@users.noreply.github.com>
2025-04-15 10:57:54 -0400

    Add MCP HTTP server for stock quote retrieval

--- MCPClient/Program.cs
+++ MCPClient/Program.cs
@@ -17,18 +17,23 @@ string modelId = azureOpenAIConfig["ModelId"]??string.Empty;
 string endpoint = azureOpenAIConfig["Endpoint"]?? string.Empty;
 string apiKey = azureOpenAIConfig["ApiKey"] ?? string.Empty;
 
-await using IMcpClient mcpClient = await McpClientFactory.CreateAsync(new StdioClientTransport(new()
+await using IMcpClient localMcpClient = await McpClientFactory.CreateAsync(new StdioClientTransport(new()
 {
     Name = "MCPServer",
     // Point the client to the MCPServer server executable
     Command = Path.Combine("..", "..", "..", "..", "MCPServer", "bin", "Debug", "net9.0", "MCPServer.exe")
 }));
 
-IList<McpClientTool> tools = await mcpClient.ListToolsAsync();
+
+await using IMcpClient remoteMcpClient = await McpClientFactory.CreateAsync(new SseClientTransport(transportOptions: new SseClientTransportOptions() { Endpoint = new Uri("http://localhost:4949/sse") }), new McpClientOptions() { ClientInfo = new() { Name="MCPHttpServer", Version="1.0.0.0" } });
+
+IList<McpClientTool> localTools = await localMcpClient.ListToolsAsync();
+IList<McpClientTool> remoteTools = await remoteMcpClient.ListToolsAsync();
 
 IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
 #pragma warning disable SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-kernelBuilder.Plugins.AddFromFunctions("Tools", tools.Select(mcpClientTool => mcpClientTool.AsKernelFunction()));
+kernelBuilder.Plugins.AddFromFunctions("LocalTools", localTools.Select(mcpClientTool => mcpClientTool.AsKernelFunction()));
+kernelBuilder.Plugins.AddFromFunctions("RemoteTools", remoteTools.Select(mcpClientTool => mcpClientTool.AsKernelFunction()));
 #pragma warning restore SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 kernelBuilder.Services.AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
 
@@ -43,7 +48,13 @@ AzureOpenAIPromptExecutionSettings executionSettings = new()
 };
 #pragma warning restore SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
-// Execute a prompt using the MCP tools. The AI model will automatically call the appropriate MCP tools to answer the prompt.
+// Execute a prompt using the MCP localTools. The AI model will automatically call the appropriate MCP localTools to answer the prompt.
 var prompt = "What is the time in EST and weather in Detroit?";
 var result = await kernel.InvokePromptAsync(prompt, new(executionSettings));
 Console.WriteLine(result);
+
+prompt = "What is the current stock price for MSFT?";
+result = await kernel.InvokePromptAsync(prompt, new(executionSettings));
+Console.WriteLine(result);
+
+Console.ReadLine();
---
+++ MCPHttpServer/Program.cs
@@ -0,0 +1,35 @@
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace MCPHttpServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services
+                .AddMcpServer()
+                .WithToolsFromAssembly();
+
+            var app = builder.Build();
+
+            app.MapGet("/", () => $"Hello MCP Server Here! {DateTime.Now}");
+            app.MapMcp();
+            app.Run("http://localhost:4949");
+        }
+    }
+
+    [McpServerToolType]
+    public static class StockQuote
+    {
+        [McpServerTool, Description("Gets the latest price for a stock symbol.")]
+        public static string GetStockQuote(string symbol)
+        {
+            var random = new Random();
+            var amount = random.Next(10000, 60001) / 100.0; // Generate random amount between 100.00 and 600.00
+            return $"${amount:F2}"; // Format as USD with two decimal places
+        }
+    }
+}
